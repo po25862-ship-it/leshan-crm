21e814f827bd8e2c433831ad304c25363d646dd4
po25862-ship-it <po25862@gmail.com>
2026-07-13 16:57:11 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// 這裡填入你在 Firebase 主控台建立專案後拿到的設定值
-// 專案設定 -> 一般 -> 你的應用程式 -> SDK 設定與程式碼
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyDiGtIemWS0C4Pb-fNFBjDnoa2Z6ETwics",
+  authDomain: "leshan-crm.firebaseapp.com",
+  projectId: "leshan-crm",
+  storageBucket: "leshan-crm.firebasestorage.app",
+  messagingSenderId: "67951666720",
+  appId: "1:67951666720:web:8c1fe1efd8579f155a3e45",
 };
 
 const app = initializeApp(firebaseConfig);
