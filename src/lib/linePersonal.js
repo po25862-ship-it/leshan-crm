8f076c30076efebc0a66cdf71a9e5350cb3d1c46
po25862-ship-it <po25862@gmail.com>
2026-08-18 07:05:47 +0800

    Add local personal LINE connector

---
+++ src/lib/linePersonal.js
@@ -0,0 +1,5 @@
+export const LINE_PERSONAL_TOOL_URL = "http://127.0.0.1:8765";
+
+export function openLinePersonalTool() {
+  window.open(LINE_PERSONAL_TOOL_URL, "_blank", "noopener,noreferrer");
+}
