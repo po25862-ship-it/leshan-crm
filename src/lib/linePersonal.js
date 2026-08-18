34d8f44248502d07c9d0d288f441863024b568b0
po25862-ship-it <po25862@gmail.com>
2026-08-18 20:47:35 +0800

    Separate LINE and FB local ports

--- src/lib/linePersonal.js
+++ src/lib/linePersonal.js
@@ -1,4 +1,5 @@
-export const LINE_PERSONAL_TOOL_URL = "http://127.0.0.1:8765";
+// 8876 專給個人 LINE；8765 保留給既有的 FB 自動分享工具。
+export const LINE_PERSONAL_TOOL_URL = "http://127.0.0.1:8876";
 
 export function openLinePersonalTool() {
   window.open(LINE_PERSONAL_TOOL_URL, "_blank", "noopener,noreferrer");
