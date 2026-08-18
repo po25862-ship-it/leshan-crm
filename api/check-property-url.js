dd6dd32e8b102c8fad1c1d90d0b259bc99e880d3
po25862-ship-it <po25862@gmail.com>
2026-08-18 22:52:36 +0800

    Use browser headers for property checks

--- api/check-property-url.js
+++ api/check-property-url.js
@@ -37,8 +37,10 @@ module.exports = async function handler(req, res) {
       redirect: "follow",
       signal: controller.signal,
       headers: {
-        "User-Agent": "Mozilla/5.0 (compatible; LeshanCRM-LinkChecker/1.0)",
-        Accept: "text/html,application/xhtml+xml",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",
+        Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "zh-TW,zh;q=0.9,en;q=0.8",
+        Referer: "https://www.twhg.com.tw/",
       },
     });
 
